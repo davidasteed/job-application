f3810e9d35515741e832afaa4b3288797f0fd8dd
David Steed <davidasteed@gmail.com>
2017-03-14 18:27:33 -0400

    corrections and improvements per instructor added.  Also added .jshintrc, ran jshint, and added docblocks

--- src/eventsData.js
+++ src/eventsData.js
@@ -1,34 +1,40 @@
 // create an iffe function to encapsulate the UI events handler function(s)
-(function iifeData() {
+(function() {
+  'use strict';
   // only create a new js namespace if it doesn't already exist
   window.jsEventHandlers = window.jsEventHandlers || {};
 
   // Function to process form submission
-  window.jsEventHandlers.submitFn = function submitFn(action) {
+  /**
+   * Ajax fetch() to submit data
+   * @param  {string}     url       fetch() url
+   * @param  {js object}  eventObj  form data from UI api
+   * @return {void}                 no return object
+   */
+  window.jsEventHandlers.submitFn = function submitFn(url, eventObj) {
     // do a fetch() to attempt to post the form data
-    //
-    //NOTE:  suppressing the following fetch failure:
-      /*
-      Fetch API cannot load http://requestb.in/1bpvy2i1.
-      No 'Access-Control-Allow-Origin' header is present on the requested resource.
-      Origin 'http://10.1.10.111:8080' is therefore not allowed access.
-      If an opaque response serves your needs,
-      set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
-      10.1.10.111/:1 Uncaught (in promise) TypeError: Failed to fetch
-       */
-    // not clear on whether the "opaque" response is truly usable,
-    // but at least I get a "failure" response
-    // that can be used to achieve half-way functional code for a bad result
+    //NOTE:  As per Jordan:  requestb.in does not accept AJAX fetch(),
+    //       so this fails as expected
     let promiseSubmit = fetch(
-      action,
+      url,
       { method: 'post',
-        mode: 'no-cors'
-      }
-    );
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        full_name: eventObj.target[1].value,
+        exp: eventObj.target[2].value,
+        languages_js: eventObj.target[3].checked,
+        languages_html: eventObj.target[4].checked,
+        languages_css: eventObj.target[5].checked,
+        previous_work_url: eventObj.target[7].value
+      })
+    }
+  );
 
-    // act on the response returned when the promise completes
-    promiseSubmit.then(function checkResponse(response){
-      window.jsEventHandlers.postResultsFn(response.status);
-    });
-  };
+  // act on the response returned when the promise completes
+  promiseSubmit.then(function checkResponse(response){
+    window.jsEventHandlers.postResultsFn(response.status);
+  });
+};
 })();
--- src/eventsUI.js
+++ src/eventsUI.js
@@ -1,52 +1,80 @@
 // create an iffe function to encapsulate the UI events handler function(s)
-(function iifeUI() {
+(function() {
+  'use strict';
   // only create a new js namespace if it doesn't already exist
   window.jsEventHandlers = window.jsEventHandlers || {};
 
   // UI Event #1:  After the user moves the cursor away from the "full name" input (that's the blur event), see if the input has a value (check the length of the value). If it does not, add a class of has-error to the ancestor section tag.
   document.querySelector('#full-name').addEventListener('blur', function blurAction(e){
-    // if the user moved the cursor away, check if there is an empty value
-    let fullName = document.querySelector('#full-name');
-    if (!fullName.value) {
-      // add has-error class to the section
-      fullName.parentNode.parentNode.classList.add('has-error');
+    if (!e.target.value) {
+        e.target.parentNode.parentNode.classList.add('has-error');
     }
-  })
+  });
+
+
+  // if the user clicks into the "full name" input element, then remove the error class
+  document.querySelector('#full-name').addEventListener('click', function clickAction(e){
+
+    if (e.target.parentNode.parentNode.classList.contains('has-error')) {
+      e.target.parentNode.parentNode.classList.remove('has-error');
+    }
+  });
 
   // UI Event #2:  When the range input ("experience") is changed, update the text in the figure with the correct value from the input. So if I slide the dot to the right 1 step the figure should read "6 years".
   // on the event listener for the range input, watch for changes to the value
   document.querySelector('#exp').addEventListener('change', function valueAction(e){
-    document.querySelector('#exp + figure').innerText = e.target.value + ' years';
+    e.target.parentNode.childNodes[3].innerText = e.target.value + ' years';
   });
 
   // UI Event #3:  When one of the "language" checkboxes is checked, increment the number of languages in the figure tag. For example, if I check "JS" and "HTML" then the figure should read: "2 languages". If I uncheck a box, the number of languages should decrease.
   // for an array of event listeners,
   // run a function on each event listener to evaluate and increment the number of languages
+
+  // numerical language counter
+  let langCounter = 0;
+
   Array.from(document.querySelectorAll('[name="languages"]'))
   .forEach(function listenerFn(checkbox){
     checkbox.addEventListener('change', function incrementFn(e){
-      // extract/store the numerical current language count
-      let currentValue = parseInt(document.querySelector('.checkbox-inline + figure').innerText);
 
-      // set the value to higher, or lower, based on whether the checkbox was checked, or unchecked
-      document.querySelector('.checkbox-inline + figure').innerText =
-      e.target.checked ? (currentValue + 1) + ' languages' : (currentValue - 1) + ' languages';
+      // set language counter figure element to indicate higher, or lower, based on whether the checkbox was checked, or unchecked.
+      if (e.target.checked === true) {
+        langCounter++;
+        // increment the numerical counter
+        e.target.parentNode.parentNode.childNodes[7].innerText = langCounter + ' languages';
+
+      } else if (e.target.checked === false){
+        // Ensure not to decrement if langCounter already equals zero.
+        if (langCounter) {
+          langCounter--;
+          // increment the numerical counter
+          e.target.parentNode.parentNode.childNodes[7].innerText = langCounter + ' languages';
+        }
+      }
     });
+    // set the language counter to equal the total
+    // document.querySelector('.checkbox-inline + figure').innerText = langCounter;
   });
 
   // UI Event #4: "Do not allow the form to be submitted, you will need to prevent the submit event from occurring."
   // Instead, when the form attempts to be submitted, call a function in your other module (you can name it whatever you want)
-  document.querySelector('form').addEventListener('submit', function submitFn(e){
+  document.querySelector('form').addEventListener('submit', function handler(e){
     // prevent the submit from being sent
     e.preventDefault();
 
     // store the action url
     let actionUrl = document.querySelector('form').action;
 
-    // call function to handle data submission, and pass th Action url
-    window.jsEventHandlers.submitFn(actionUrl);
+    // call function to handle data submission,
+    // and pass the Action url and the eventListener object
+    window.jsEventHandlers.submitFn(actionUrl, e);
   });
 
+  /**
+   * [postResultsFn description:  handle error reporting after form submission]
+   * @param  {number} code [the response.status from the fetch post()]
+   * @return {void}        [nothing is returned]
+   */
   window.jsEventHandlers.postResultsFn = function postResultsFn(code) {
     let resultsSel = document.querySelector('#results');
     if (code > 199 && code < 300) {
@@ -54,7 +82,7 @@
       resultsSel.style.display = 'block';
       // add "success message" inside the results element
       resultsSel.innerText =
-      "Your application has been received!";
+      'Your application has been received!';
     } else {
       // change the class to "has-error"
       resultsSel.classList.add('has-error');
@@ -62,7 +90,7 @@
       resultsSel.style.display = 'block';
       // add "failure message" inside the results element
       resultsSel.innerText =
-      "Your application failed with HTTP POST result code: " + code;
+      'Your application failed with HTTP POST result code: ' + code;
     }
-  }
+  };
 })();
